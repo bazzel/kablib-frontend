27891f542aa5edb569e7aac872ca9b3005d1a88c
Patrick Baselier <patrick@kabisa.nl>
2014-10-07 19:19:52 +0200

    Update book

--- app/controllers/books/show.js
+++ app/controllers/books/show.js
@@ -6,6 +6,14 @@ export default Ember.ObjectController.extend({
     toggleEditing: function() {
       this.toggleProperty('isEditing');
       this.get('model').rollback();
+    },
+    submit: function() {
+      var book = this.model,
+         _this = this;
+
+      book.save().then(function() {
+        _this.set('isEditing', false);
+      });
     }
   }
 });
