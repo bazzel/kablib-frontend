dc290fc708f7a88ae88dd9540fa8e2ee7e13f0d5
Patrick Baselier <patrick@kabisa.nl>
2014-10-08 15:19:04 +0200

    Add search to login

--- app/controllers/login.js
+++ app/controllers/login.js
@@ -1,10 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.ArrayController.extend({
+  search: '',
   actions: {
     login: function(user) {
-      this.set('currentUser', user);
       var previousTransition = this.get('previousTransition');
+
+      this.set('currentUser', user);
+
       if (previousTransition) {
         this.set('previousTransition', null);
         previousTransition.retry();
@@ -12,6 +15,14 @@ export default Ember.ArrayController.extend({
         // Default back to homepage
         this.transitionToRoute('index');
       }
+      this.set('search', '');
     }
-  }
+  },
+  filteredContent: function() {
+    var search = new RegExp(this.get('search'), 'gi');
+
+    return this.model.filter(function(user) {
+      return search.test(user.get('fullName'));
+    });
+  }.property('@each', 'search')
 });
