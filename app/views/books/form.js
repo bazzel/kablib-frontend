d36967cc7c48f95a01195b043678f7c0252749f9
Patrick Baselier <patrick@kabisa.nl>
2014-10-14 21:39:01 +0200

    Use render i.o. view

--- app/views/books/form.js
+++ app/views/books/form.js
@@ -1,7 +1,6 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
-  templateName: 'books/form',
   scheduleSelectize: function() {
     Ember.run.scheduleOnce('afterRender', this, 'applySelectize');
   }.on('didInsertElement', 'controller.isEditing'),
