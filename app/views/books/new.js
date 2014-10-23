ba424d94fbdcbe215f31880b6fa94104a97b0932
Patrick Baselier <patrick@kabisa.nl>
2014-10-23 08:53:33 +0200

    Get rid of affix

--- app/views/books/new.js
+++ app/views/books/new.js
@@ -1,11 +1,5 @@
 import Ember from 'ember';
 
 export default Ember.View.extend({
-  classNames: ['panel', 'panel-default', 'book'],
-  schedulePositioning: function() {
-    Ember.run.scheduleOnce('afterRender', this, 'applyPositioning');
-  }.on('didInsertElement'),
-  applyPositioning: function() {
-    this.$().affix();
-  }
+  classNames: ['panel', 'panel-default', 'book']
 });
